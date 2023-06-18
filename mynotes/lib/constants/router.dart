cc5558e44f37a9d1285ef0e7ed5e1a8da7d2e8dc
Adrian Włodarczyk <33695278+Shade644@users.noreply.github.com>
2023-06-18 12:49:29 +0200

    Create New Notes
    .

--- mynotes/lib/constants/router.dart
+++ mynotes/lib/constants/router.dart
@@ -1,4 +1,5 @@
 const loginRoute='/login/';
 const registerRoute='/register/';
 const notesRoute='/notes/';
-const verifyemailRoute='/verify-email';
+const verifyemailRoute='/verify-email/';
+const newnotesRoute='/notes/new-note/';
